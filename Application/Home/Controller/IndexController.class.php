e9626d878600f588f5eee001cd4892656a40a1ae
朱瑞恒 <18749830459@163.com>
2017-08-16 09:51:14 +0800

    登录

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,20 +4,20 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-		$this->show();
-    }
+		 $this->show();
+     }
 //login
 
-   function login(){
+function login(){
     if($_POST){
       //print_r($_POST);die;
-        I('request.tel')?$_POST['tel']=I('request.tel'):jsonapp_fun(-1,'电话不能为空');
+        I('request.iphone')?$_POST['iphone']=I('request.iphone'):jsonapp_fun(-1,'电话不能为空');
         
         I('request.dizhi')?$_POST['dizhi']=I('request.dizhi'):jsonapp_fun(-1,'地址不能为空');
                    
            $add=M('user')->add($_POST);
                 if($add){
-                     $this->display('Index/taocan');
+                     $this->display('Index/index');
                 }else{
                     jsonapp_fun(1,'注册失败');
                 }
